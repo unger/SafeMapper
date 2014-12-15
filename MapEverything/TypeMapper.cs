e62bfc39e6f48318bf54775c07ca699556e877ef
Magnus Unger <magnus_unger@hotmail.com>
2014-12-16 00:03:04 +0100

    initial commit

---
+++ MapEverything/TypeMapper.cs
@@ -0,0 +1,54 @@
+﻿namespace MapEverything
+{
+    using System;
+    using System.Diagnostics;
+    using System.Globalization;
+    using System.Reflection;
+
+    public class TypeMapper
+    {
+        public TypeMapper()
+        {
+            
+        }
+
+        public object ConvertTo(object value, Type type)
+        {
+            return this.ConvertTo(value, type, CultureInfo.CurrentCulture);
+        }
+
+        public object ConvertTo(object value, Type type, IFormatProvider formatProvider)
+        {
+            if (value == null)
+            {
+                return this.GetDefaultValue(type);
+            }
+
+            var converter = this.FindConverter(value.GetType(), type, formatProvider);
+
+
+            return converter.Invoke(null, new[] { value });
+        }
+
+        private MethodInfo FindConverter(Type sourceType, Type destinationType, IFormatProvider formatProvider)
+        {
+            var parseMethod = destinationType.GetMethod("Parse", new Type[] { sourceType });
+            if (parseMethod != null)
+            {
+                return parseMethod;
+            }
+
+            return null;
+        }
+
+        private object GetDefaultValue(Type t)
+        {
+            if (t.IsValueType)
+            {
+                return Activator.CreateInstance(t);
+            }
+
+            return null;
+        }
+    }
+}
