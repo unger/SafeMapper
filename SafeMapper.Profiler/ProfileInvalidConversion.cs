dabf60ccd36b97e358d2cba7437f0edfb4f6cd25
Magnus Unger <magnus_unger@hotmail.com>
2015-06-28 15:51:26 +0200

    Fixed warning

--- SafeMapper.Profiler/ProfileInvalidConversion.cs
+++ SafeMapper.Profiler/ProfileInvalidConversion.cs
@@ -64,7 +64,7 @@
                     {
                         action(i);
                     }
-                    catch (Exception e)
+                    catch (Exception)
                     {
                     }
                 };
