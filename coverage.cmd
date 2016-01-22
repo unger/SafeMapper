de07939f33caf7c6f7542ec1cbd28ec1aa2406a7
Magnus Unger <magnus_unger@hotmail.com>
2016-01-22 21:49:11 +0100

    Installed solution level packages to Tests project to support VS 2015

--- coverage.cmd
+++ coverage.cmd
@@ -4,6 +4,6 @@ REM packages\OpenCover.4.6.166\tools\OpenCover.Console.exe -register:user -targe
 
 packages\OpenCover.4.6.166\tools\OpenCover.Console.exe -register:user -target:packages\NUnit.Runners.2.6.4\tools\nunit-console-x86.exe -targetargs:"SafeMapper.Tests\bin\Debug\SafeMapper.Tests.dll /noshadow /xml=coverage\TestResult.xml" -filter:"+[SafeMapper*]* -[SafeMapper.Tests*]*" -output:coverage\opencovertests.xml
 
-packages\ReportGenerator.2.3.2.0\tools\ReportGenerator.exe -reports:"coverage\opencovertests.xml" -targetdir:"coverage"
+packages\ReportGenerator.2.4.0.0\tools\ReportGenerator.exe -reports:"coverage\opencovertests.xml" -targetdir:"coverage"
 
 pause
